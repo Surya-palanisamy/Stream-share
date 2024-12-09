59d04611a3962bd14f9f45611b917cf3da8ae885
Surya-palanisamy <suryasitit@gmail.com>
2024-12-09 14:24:08 +0530

    first commit

--- server/src/server.ts
+++ server/src/server.ts
@@ -11,26 +11,16 @@ dotenv.config();
 
 const app = express();
 
-// Middleware
 app.use(express.json());
-app.use(
-  cors({
-    origin: "*", // Allow the frontend URL here
-    methods: ["GET", "POST"],
-    allowedHeaders: ["Content-Type"],
-    credentials: true,
-  })
-);
 
-app.use(express.static(path.join(__dirname, "public"))); // Serve static files if any
+app.use(cors());
+
+app.use(express.static(path.join(__dirname, "public"))); // Serve static files
 
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "*", // Update this to your frontend URL
-    methods: ["GET", "POST"],
-    allowedHeaders: ["Content-Type"],
-    credentials: true,
+    origin: "*",
   },
   maxHttpBufferSize: 1e8,
   pingTimeout: 60000,
@@ -38,11 +28,12 @@ const io = new Server(server, {
 
 let userSocketMap: User[] = [];
 
-// Utility functions
+// Function to get all users in a room
 function getUsersInRoom(roomId: string): User[] {
   return userSocketMap.filter((user) => user.roomId == roomId);
 }
 
+// Function to get room id by socket id
 function getRoomId(socketId: SocketId): string | null {
   const roomId = userSocketMap.find(
     (user) => user.socketId === socketId
@@ -64,9 +55,10 @@ function getUserBySocketId(socketId: SocketId): User | null {
   return user;
 }
 
-// Socket.IO logic
 io.on("connection", (socket) => {
+  // Handle user actions
   socket.on(SocketEvent.JOIN_REQUEST, ({ roomId, username }) => {
+    // Check is username exist in the room
     const isUsernameExist = getUsersInRoom(roomId).filter(
       (u) => u.username === username
     );
@@ -91,7 +83,6 @@ io.on("connection", (socket) => {
     io.to(socket.id).emit(SocketEvent.JOIN_ACCEPTED, { user, users });
   });
 
-  // Handle user disconnecting
   socket.on("disconnecting", () => {
     const user = getUserBySocketId(socket.id);
     if (!user) return;
@@ -113,24 +104,95 @@ io.on("connection", (socket) => {
     }
   );
 
-  // Other event listeners for file and directory changes, chat, typing, etc.
   socket.on(SocketEvent.DIRECTORY_CREATED, ({ parentDirId, newDirectory }) => {
+    const roomId = getRoomId(socket.id);
+    if (!roomId) return;
+    socket.broadcast.to(roomId).emit(SocketEvent.DIRECTORY_CREATED, {
+      parentDirId,
+      newDirectory,
+    });
+  });
+
+  socket.on(SocketEvent.DIRECTORY_UPDATED, ({ dirId, children }) => {
+    const roomId = getRoomId(socket.id);
+    if (!roomId) return;
+    socket.broadcast.to(roomId).emit(SocketEvent.DIRECTORY_UPDATED, {
+      dirId,
+      children,
+    });
+  });
+
+  socket.on(SocketEvent.DIRECTORY_RENAMED, ({ dirId, newName }) => {
+    const roomId = getRoomId(socket.id);
+    if (!roomId) return;
+    socket.broadcast.to(roomId).emit(SocketEvent.DIRECTORY_RENAMED, {
+      dirId,
+      newName,
+    });
+  });
+
+  socket.on(SocketEvent.DIRECTORY_DELETED, ({ dirId }) => {
+    const roomId = getRoomId(socket.id);
+    if (!roomId) return;
+    socket.broadcast.to(roomId).emit(SocketEvent.DIRECTORY_DELETED, { dirId });
+  });
+
+  socket.on(SocketEvent.FILE_CREATED, ({ parentDirId, newFile }) => {
     const roomId = getRoomId(socket.id);
     if (!roomId) return;
     socket.broadcast
       .to(roomId)
-      .emit(SocketEvent.DIRECTORY_CREATED, { parentDirId, newDirectory });
+      .emit(SocketEvent.FILE_CREATED, { parentDirId, newFile });
   });
 
   socket.on(SocketEvent.FILE_UPDATED, ({ fileId, newContent }) => {
     const roomId = getRoomId(socket.id);
     if (!roomId) return;
-    socket.broadcast
-      .to(roomId)
-      .emit(SocketEvent.FILE_UPDATED, { fileId, newContent });
+    socket.broadcast.to(roomId).emit(SocketEvent.FILE_UPDATED, {
+      fileId,
+      newContent,
+    });
   });
 
-  // Other events here...
+  socket.on(SocketEvent.FILE_RENAMED, ({ fileId, newName }) => {
+    const roomId = getRoomId(socket.id);
+    if (!roomId) return;
+    socket.broadcast.to(roomId).emit(SocketEvent.FILE_RENAMED, {
+      fileId,
+      newName,
+    });
+  });
+
+  socket.on(SocketEvent.FILE_DELETED, ({ fileId }) => {
+    const roomId = getRoomId(socket.id);
+    if (!roomId) return;
+    socket.broadcast.to(roomId).emit(SocketEvent.FILE_DELETED, { fileId });
+  });
+
+  // Handle user status
+  socket.on(SocketEvent.USER_OFFLINE, ({ socketId }) => {
+    userSocketMap = userSocketMap.map((user) => {
+      if (user.socketId === socketId) {
+        return { ...user, status: USER_CONNECTION_STATUS.OFFLINE };
+      }
+      return user;
+    });
+    const roomId = getRoomId(socketId);
+    if (!roomId) return;
+    socket.broadcast.to(roomId).emit(SocketEvent.USER_OFFLINE, { socketId });
+  });
+
+  socket.on(SocketEvent.USER_ONLINE, ({ socketId }) => {
+    userSocketMap = userSocketMap.map((user) => {
+      if (user.socketId === socketId) {
+        return { ...user, status: USER_CONNECTION_STATUS.ONLINE };
+      }
+      return user;
+    });
+    const roomId = getRoomId(socketId);
+    if (!roomId) return;
+    socket.broadcast.to(roomId).emit(SocketEvent.USER_ONLINE, { socketId });
+  });
 
   // Handle chat actions
   socket.on(SocketEvent.SEND_MESSAGE, ({ message }) => {
@@ -138,14 +200,64 @@ io.on("connection", (socket) => {
     if (!roomId) return;
     socket.broadcast.to(roomId).emit(SocketEvent.RECEIVE_MESSAGE, { message });
   });
+
+  // Handle cursor position
+  socket.on(SocketEvent.TYPING_START, ({ cursorPosition }) => {
+    userSocketMap = userSocketMap.map((user) => {
+      if (user.socketId === socket.id) {
+        return { ...user, typing: true, cursorPosition };
+      }
+      return user;
+    });
+    const user = getUserBySocketId(socket.id);
+    if (!user) return;
+    const roomId = user.roomId;
+    socket.broadcast.to(roomId).emit(SocketEvent.TYPING_START, { user });
+  });
+
+  socket.on(SocketEvent.TYPING_PAUSE, () => {
+    userSocketMap = userSocketMap.map((user) => {
+      if (user.socketId === socket.id) {
+        return { ...user, typing: false };
+      }
+      return user;
+    });
+    const user = getUserBySocketId(socket.id);
+    if (!user) return;
+    const roomId = user.roomId;
+    socket.broadcast.to(roomId).emit(SocketEvent.TYPING_PAUSE, { user });
+  });
+
+  socket.on(SocketEvent.REQUEST_DRAWING, () => {
+    const roomId = getRoomId(socket.id);
+    if (!roomId) return;
+    socket.broadcast
+      .to(roomId)
+      .emit(SocketEvent.REQUEST_DRAWING, { socketId: socket.id });
+  });
+
+  socket.on(SocketEvent.SYNC_DRAWING, ({ drawingData, socketId }) => {
+    socket.broadcast
+      .to(socketId)
+      .emit(SocketEvent.SYNC_DRAWING, { drawingData });
+  });
+
+  socket.on(SocketEvent.DRAWING_UPDATE, ({ snapshot }) => {
+    const roomId = getRoomId(socket.id);
+    if (!roomId) return;
+    socket.broadcast.to(roomId).emit(SocketEvent.DRAWING_UPDATE, {
+      snapshot,
+    });
+  });
 });
 
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req: Request, res: Response) => {
+  // Send the index.html file
   res.sendFile(path.join(__dirname, "..", "public", "index.html"));
 });
 
 server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Listening on port ${PORT}`);
 });
