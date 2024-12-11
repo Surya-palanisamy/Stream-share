2485e5b60860db3c594ceb294d51455bf648cb75
Surya-palanisamy <suryasitit@gmail.com>
2024-12-11 16:48:57 +0530

    fix: react is removed in runview.tsx

--- client/src/components/sidebar/sidebar-views/RunView.tsx
+++ client/src/components/sidebar/sidebar-views/RunView.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { useRunCode } from "@/context/RunCodeContext";
 import useResponsive from "@/hooks/useResponsive";
 import { ChangeEvent } from "react";
